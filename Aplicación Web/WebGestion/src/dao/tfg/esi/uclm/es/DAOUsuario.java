04b72f925afcf56cb2a69d53083106cf728fff23
Diego Andérica <dar-96@hotmail.com>
2018-06-18 18:11:12 +0200

    Reorganización del código de cifrado

--- Aplicación Web/WebGestion/src/dao/tfg/esi/uclm/es/DAOUsuario.java
+++ Aplicación Web/WebGestion/src/dao/tfg/esi/uclm/es/DAOUsuario.java
@@ -2,17 +2,25 @@ package dao.tfg.esi.uclm.es;
 
 import java.security.MessageDigest;
 
-public class DAOUsuario {	
+public class DAOUsuario {
 	public static String encriptar(String pwd) throws Exception {
-		MessageDigest md = MessageDigest.getInstance("SHA1");
-        StringBuffer sb = new StringBuffer();
-        
-        byte[] result = md.digest(pwd.getBytes());
-
-        for (int i = 0; i < result.length; i++) {
-            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
-        }
-
-        return sb.toString();
+		MessageDigest md;
+		StringBuffer sb = new StringBuffer();
+		byte[] result;
+		
+		try {
+			md = MessageDigest.getInstance("SHA1");
+	        
+	        result = md.digest(pwd.getBytes());
+	
+	        for (int i = 0; i < result.length; i++) {
+	            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+	        }
+	        
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return sb.toString();
 	}
 }
