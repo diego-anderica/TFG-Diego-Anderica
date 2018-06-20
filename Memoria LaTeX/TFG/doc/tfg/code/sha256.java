04f2cc0cb10c0281c87889fb183ee6c4f1842781
Diego Andérica <dar-96@hotmail.com>
2018-06-20 20:14:02 +0200

    Avances en la memoria 20-06-18

--- Memoria LaTeX/TFG/doc/tfg/code/sha256.java
+++ Memoria LaTeX/TFG/doc/tfg/code/sha256.java
@@ -1,21 +1,23 @@
 public static String encriptar(String pwd) throws Exception {
-	MessageDigest md;
 	StringBuffer sb = new StringBuffer();
+	MessageDigest md;
 	byte[] result;
 	
 	try {
 		md = MessageDigest.getInstance("SHA-256");
-		result = md.digest(pwd.getBytes());
+		md.reset(); // Resetear digest
+		md.update(pwd.getBytes()); // Actualizar digest con contraseña a encriptar
 
+		result = md.digest(); // Obtener digest de la contraseña
+
+		// Convertir resultado a cadena hexadecimal
 		for (int i = 0; i < result.length; i++) {
 			sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
 		}
-        
+		
 	} catch (Exception e) {
 		e.printStackTrace();
 	}
 	
 	return sb.toString();
 }
-
-// Fuente: http://www.sha1-online.com/sha256-java/
